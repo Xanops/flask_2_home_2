478eb6f3fd9a6ddf8d5b89a607d4a8a913551796
Xanops <pzvpetr@gmail.com>
2021-03-30 00:27:08 +0300

    it's works!!!

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
-    return render_template('page.html', title=title)
+    return render_template('base.html', title=title)
 
 
 @app.route('/table/<gender>/<age>')
